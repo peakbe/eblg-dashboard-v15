b3802bcda819c5ac4add85c41437345c221a3185
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-04-02 14:50:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
+// ======================================================
+// IMPORTS
+// ======================================================
+
+// Carte
 import { initMap } from "./js/map.js";
+
+// Runways (important : doit être chargé avant METAR)
 import "./js/runways.js";
+
+// METAR / TAF / FIDS
 import { loadMetar } from "./js/metar.js";
 import { loadTaf } from "./js/taf.js";
 import { loadFids } from "./js/fids.js";
+
+// UI
 import { initUI } from "./js/ui.js";
+
+// Heatmap
 import { initHeatmapToggle, initHeatmapHistory } from "./js/sonometers.js";
 
+
+// ======================================================
+// INITIALISATION
+// ======================================================
+
 window.onload = () => {
+
+    // 1) Carte
     window.map = initMap();
+
+    // 2) UI
     initUI();
+
+    // 3) Heatmap
     initHeatmapToggle(window.map);
     initHeatmapHistory(window.map);
+
+    // 4) Données
     loadMetar();
     loadTaf();
     loadFids();
